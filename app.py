3cefe526a131760f585d41d2504244cab12cbb25
exqt <exqt_@naver.com>
2018-06-29 13:30:48 +0900

    Change post part to classify image

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, send_from_directory, json, jsonify
 
 import breeds
+from doggyML.dog_classify import classify_image
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -14,11 +15,12 @@ def create_app(test_config=None):
     def index():
         if not 'image' in request.files:
             content = {'error': 'please send an image'}
-            return content, 400 
+            return content, 400
        
         file = request.files['image']
         file.save(file.filename)
-        return 'OK'
+
+        return classify_image(file.filename)
 
     @app.route('/static/<path:path>', methods=['GET'])
     def send_static(path):
